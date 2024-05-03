95022f3845b1f39dff7a752b7f6b9b94027ada52
juan9walteros <80785635+Juan9Walteros@users.noreply.github.com>
2024-05-02 19:26:11 -0500

    Creación proyecto en react, con dependecias en react booststrap y  react reouter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import NavBarPeakU from './layouts/navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarPeakU />
+    </div>
+  );
+}
+
+export default App;
